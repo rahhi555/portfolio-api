fead8decc6e8fb2990f8cf63f9dd194e35081070
rahhi555 <whowhose0904@gmail.com>
2021-08-15 01:19:55 +0900

    Add: userモデル追加

---
+++ db/migrate/20210814161222_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :uid, null: false, index: { unique: true }
+
+      t.timestamps
+    end
+  end
+end
