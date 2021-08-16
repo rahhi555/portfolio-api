90e920bbe5c479e97e2bc489d11516c2f114b831
rahhi555 <whowhose0904@gmail.com>
2021-08-17 01:06:00 +0900

    Add: requiest spec for User#create

--- config/initializers/firebase_auth_initializer.rb
+++ config/initializers/firebase_auth_initializer.rb
@@ -1,4 +1,4 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(host: '172.17.0.2')
+  config.redis = Redis.new(host: ENV.fetch("RAILS_REDIS_HOST") { 'redis' }, db: 15)
   config.project_ids = ['minimap-dadd2']
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,19 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
   describe "Get api/v1/users/:id" do
-    let(:headers) {
-      {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer token'
-      }
-    }
-    let(:user) { FactoryBot.create(:user) }
+    let(:user) { create(:user) }
 
     before { stub_firebase(user) }
 
     context "有効なidの場合" do
       it "ユーザーを取得できること" do
-        get api_v1_user_path(user), headers: headers
+        get api_v1_user_path(user)
         expect(response).to have_http_status(200)
         expect(JSON.parse(response.body)['id']).to eq user.id
       end
@@ -22,12 +16,19 @@ RSpec.describe "Api::V1::Users", type: :request do
   end
 
   describe "Post api/v1/users" do
+    let(:user) { build(:user) }
+
     context "有効な属性値の場合" do
       it "ユーザーを追加できること" do
         FirebaseIdToken.test!
+        payload = JSON.parse File.read("#{Rails.root}/spec/factories/files/payload.json")
+        payload = JWT.encode payload, OpenSSL::PKey::RSA.new(FirebaseIdToken::Testing::Certificates.private_key), 'RS256'
 
-        post api_v1_users_path, params: { user: { name: 'テストネーム' } }, headers: headers
+        expect{
+          post api_v1_users_path, params: { user: { name: 'テストネーム' } }, headers: { Authorization: "Bearer #{payload}" }
+        }.to change{ User.count }.by(1)
         expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)['name']).to eq 'テストネーム'
       end
     end
   end
