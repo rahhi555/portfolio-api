0bbd739eed1f6ab845a0e440a16968ce61d4bed4
rahhi555 <whowhose0904@gmail.com>
2021-08-27 07:51:01 +0900

    Add: plansテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_161222) do
+ActiveRecord::Schema.define(version: 2021_08_26_223709) do
+
+  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_08_14_161222) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
+  add_foreign_key "plans", "users"
 end
---
+++ spec/factories/plans.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :plan do
+    name { "MyString" }
+    user { "" }
+  end
+end
