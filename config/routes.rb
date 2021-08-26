f263f43c99eb9fb62e1ac520bf467d0b30f20003
rahhi555 <whowhose0904@gmail.com>
2021-08-27 00:17:47 +0900

    Fix: 開発環境の認証を緩和した

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get '/health-check', to: 'health_check#health_check'
   namespace 'api', format: 'json' do
     namespace 'v1' do
-      resources 'users', only: %i[show create] do
-        get 'me', on: :collection
-      end
+      resources 'users', only: %i[index show create]
+      get 'me', to: 'users#me'
+      delete 'users', to: 'users#destroy'
+      patch 'users', to: 'users#update'
     end
   end
 end
