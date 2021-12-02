e438e6568a6f0d7f53b39f3036ff94b2b58c627a
rahhi555 <whowhose0904@gmail.com>
2021-12-02 10:48:13 +0900

    Fix: ActionCableの計画実行及び終了を二重に実行できないよう修正

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,5 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    rescue_from RuntimeError, with: :reject
   end
 end
--- app/channels/plan_channel.rb
+++ app/channels/plan_channel.rb
@@ -2,28 +2,32 @@ class PlanChannel < ApplicationCable::Channel
   def subscribed
     @plan = Plan.find(params[:plan_id])
     stream_for @plan
+    Rails.logger.info @plan
   rescue ActiveRecord::RecordNotFound
     reject
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 
   def changeTodoStatus(req)
+    req['id']
     todo_status = TodoStatus.find(req['id'])
     todo_status.update!(status: req['status'])
     PlanChannel.broadcast_to(@plan, { action: req['action'], id: req['id'], status: req['status'] })
   end
 
   def activatePlan(req)
-    return if @plan.active?
+    reject if @plan.active?
 
     todo_statuses = @plan.activate
     PlanChannel.broadcast_to(@plan, { action: req['action'], todoStatuses: todo_statuses })
   end
 
   def inactivatePlan(req)
+    reject unless @plan.active?
+
     @plan.inactivate
     PlanChannel.broadcast_to(@plan, { action: req['action'] })
   end
