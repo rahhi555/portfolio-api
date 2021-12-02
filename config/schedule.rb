839a1d83745f35bb3ee86c2e009b2b1440a6cbfe
rahhi555 <whowhose0904@gmail.com>
2021-12-03 04:48:43 +0900

    Add: 作成したrakeをschedule.rbに反映し、動作確認

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,10 +4,17 @@ set :environment, Rails.env.to_sym
 
 set :output, "#{Rails.root}/log/cron.log"
 
+# crontab生存確認用
+every 1.hours do
+  command 'echo $(date)'
+end
+
+# firebase証明書を1時間おきに取得
 every 1.hours do
   rake 'firebase:certificates:force_request'
 end
 
-every 1.hours do
-  command 'echo $(date)'
+# activeにして一日経過した計画をinactivateする
+every 1.day, at: '3:00 am' do
+  rake 'plans:inactivate_plans_since_yesterday'
 end
